65c350d84d192fdedfa4d6e22eb7a3523d4cd8b1
Behlouli Belaid <belaid.behlouli@edu.devinci.fr>
2023-07-07 17:02:00 +0200

    final update

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ async function getDataFromAPI() {
   return await fetch("https://data.culture.gouv.fr/api/records/1.0/search/?dataset=panorama-des-festivals&q=&rows=20")
     .then(response => response.json())
     .then(data => {
-      // Retourner les données de l'API
+    
       return data;
     })
     .catch(error => {
-      // Gérer les erreurs éventuelles
+    
       console.log(error);
     });
 }
